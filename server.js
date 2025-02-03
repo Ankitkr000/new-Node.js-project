ab8dfa93b58ac0c2b0233adb6ebc8c0edbbe8942
Ankitkr <ankitkr1001a@gmail.com>
2025-02-03 11:26:24 +0530

    done

--- server.js
+++ server.js
@@ -5,14 +5,7 @@ const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect("your_database_url", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.error("MongoDB connection error:", err);
-});
+
 
 const userRoutes = require("./routes/userRoutes");
 app.use("/api", userRoutes);
